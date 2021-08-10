625bfe3b1547aa375ff8f6db5d9a8c7c0b688f92
Vini <viniciusnapolitano75@gmail.com>
2021-08-10 08:24:03 -0300

    :package: calculo do orcamento

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
 document.querySelector(".hamburguer").addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu")
 })
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function() {
+    const prazo = document.querySelector("#prazo").value
+    let semanas = 'semana'
+    
+    if (prazo > 1) semanas = 'semanas'
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo de ${prazo} ${semanas}`
+    atualizarPreco()
+})
+
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const prazo = document.querySelector("#prazo").value
+    const temJS = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
